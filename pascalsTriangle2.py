bd09466416b9cdd260277d8e4f7f649644f1851c
samuelnba <samuelhalamadeveloper@gmail.com>
2022-06-19 21:56:23 +0200

    pascalsTriangle2 finished

--- pascalsTriangle2.py
+++ pascalsTriangle2.py
@@ -1,5 +1,18 @@
 def getrow(rowIndex):
     triangle = [[1]]
     for i in range(rowIndex):
-        print(i)
+        triangle.append([])
+        for j in range(len(triangle[i]) + 1):
+            triangle[i + 1].append(0)
+        for j in range(len(triangle[i+1])):
+            if j-1 < 0:
+                triangle[i+1][j] = triangle[i][j]
+            elif j == len(triangle[i+1]) - 1:
+                triangle[i+1][j] = triangle[i][j-1]
+            else:
+                triangle[i+1][j] = triangle[i][j-1] + triangle[i][j]
+    return triangle
 
+print(getrow(4))
+
+# Better than 13% of people
